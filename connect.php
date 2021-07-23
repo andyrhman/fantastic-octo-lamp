33b3d3272cc337f4ae671fd09f5d06b58ef60ed9
Andy Rahman <andyrahman989@gmail.com>
2021-07-23 21:34:09 +0800

    adakah

--- connect.php
+++ connect.php
@@ -8,5 +8,5 @@ $db_name = 'blog';
 $conn = new MySQLi($host, $user, $pass, $db_name);
 
 if($conn->connect_error){
-    die('Database connection errorrrrrrrrrrrrrrrrrrr: '. $conn->connect_error);
+    die('Database connection error: '. $conn->connect_error);
 }
